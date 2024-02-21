d68a9bdc00954a1bb719f8992aeaf39877da3c07
Marco Cerrato <marco.cerrato@spindox.it>
2024-02-21 16:56:27 +0100

    bug fix

--- src/main/java/it/marco/MainTest.java
+++ src/main/java/it/marco/MainTest.java
@@ -4,4 +4,8 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("OKKKKKK");
 	}
+	
+	public void eval(String expr) {
+		System.out.println("expr "+expr);
+	}
 }
